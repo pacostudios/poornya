b1ea3e192b0d07efcff3bb493d65b1e72b5cf408
Jayasurya-T-Appu <pacostudios.work@gmail.com>
2026-04-18 19:18:41 +0530

    Jayasurya:  email update

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -6,8 +6,8 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   auth: {
-    user:  process.env.EMAIL_USER || "info@poornaya.com" ,
-    pass:  process.env.EMAIL_PASS || "Poornaya@123" ,
+    user: "info@poornaya.com",
+    pass: "Poornaya@123",
   },
 });
 
